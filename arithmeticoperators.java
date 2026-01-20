080a971f7d4d94c7ad48d53ed7495ca5588c0d83
Manjima-26 <manjimamurali2610@gmail.com>
2026-01-20 20:03:05 +0530

    Replace user input with hardcoded values

--- arithmeticoperators.java
+++ arithmeticoperators.java
@@ -1,10 +1,9 @@
-import java.util.Scanner;
 public class arithmeticoperators
 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		int num1=scan.nextInt();
-		int num2=scan.nextInt();
+		int num1=70;
+		int num2=60;
 		System.out.println("the addition of two numbers:"+ (num1+num2));
 		System.out.println("the subraction  of two numbers:"+ (num1-num2));
 		System.out.println("the multiplication of two numbers:"+ (num1*num2));
