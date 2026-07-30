dda431e35709685e9cd5b091cbcc53051ffb07f9
Manjima M <manjimamurali2610@gmail.com>
2026-07-30 10:07:22 +0530

    Add files via upload

---
+++ elseifgrades.java
@@ -0,0 +1,19 @@
+public class elseifgrades
+{
+	public static void main(String[] args) {
+		int mark = 80;   
+
+        if (mark >= 90) {
+            System.out.println("A Grade");
+        }
+        else if (mark >= 75) {
+            System.out.println("B Grade");
+        }
+        else if (mark >= 50) {
+            System.out.println("C Grade");
+        }
+        else {
+            System.out.println("Fail");
+        }
+	}
+}
