67d0c86ced8e4cc427cf984c9616f0ede4da9869
Manjima M <manjimamurali2610@gmail.com>
2026-07-30 10:10:10 +0530

    Add files via upload

---
+++ ifelsegreaterthanandlessthan.java
@@ -0,0 +1,12 @@
+public class ifelsegreaterthanandlessthan
+{
+	public static void main(String[] args) {
+		int a=16;
+	    if(a>10 && a<50){
+	        System.out.println("Number is greater than 10 and less than 50");
+	    }
+	    else {
+	        System.out.println("Number is not in the range 10 to 50");
+	    }
+	}
+}
