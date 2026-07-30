ab7b13711d538c77647f70cf7bce936fa432b46b
Manjima M <manjimamurali2610@gmail.com>
2026-07-30 10:55:09 +0530

    Add files via upload

---
+++ MergeArray.java
@@ -0,0 +1,12 @@
+public class MergeArray {
+    public static void main(String[] args) {
+        int[] a = {1, 3, 5};
+        int[] b = {2, 4, 6};
+
+        for (int i : a)
+            System.out.print(i + " ");
+
+        for (int i : b)
+            System.out.print(i + " ");
+    }
+}
