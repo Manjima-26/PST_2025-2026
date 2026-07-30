0c0f04d8acca1a455ba8ba562984a93d4bbbd317
Manjima M <manjimamurali2610@gmail.com>
2026-07-30 10:10:30 +0530

    Add files via upload

---
+++ ifelseoddoreven.java
@@ -0,0 +1,12 @@
+public class ifelseoddoreven
+{
+	public static void main(String[] args) {
+		int num = 78;
+
+        if (num % 2 == 0) {
+            System.out.println("Number is even");
+        } else {
+            System.out.println("Number is odd");
+        }
+	}
+}
