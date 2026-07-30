85f1debb5af7df888bad34790f36b397b809fbbf
Manjima M <manjimamurali2610@gmail.com>
2026-07-30 10:09:29 +0530

    Add files via upload

---
+++ ifelsegracemark.java
@@ -0,0 +1,13 @@
+public class ifelsegracemark
+{
+	public static void main(String[] args) {
+		int student1=18;
+	    int grace=2;
+	    if(student1+grace>=40){
+	        System.out.println("student1 pass ");
+	    }
+	    else {
+	        System.out.println("student1 fail");
+	    }
+	}
+}
